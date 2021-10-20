0c91deda988b5b6e35091da71e6973e71a479350
devy688 <devyoung688@gmail.com>
2021-10-20 15:17:16 +0900

    Add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -3,45 +3,19 @@ import VideoList from "./components/video_list/video_list";
 import styles from "./app.module.css";
 import { useEffect, useState } from "react";
 
-const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
-
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${API_KEY}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
 
-  /* useEffect */
-  // dependency array를 생략하면 state나 props이 업데이트 될 때마다 등록한 콜백함수를 호출
-  // 빈 배열을 등록해두면 한 번만, 마운트 되었을 때 호출
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${API_KEY}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
 
   return (
