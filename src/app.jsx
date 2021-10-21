5745e876e7a95a1ea0ee9927a62bb59629878dcf
devy688 <devyoung688@gmail.com>
2021-10-21 19:15:01 +0900

    Remove comment

--- src/app.jsx
+++ src/app.jsx
@@ -4,9 +4,6 @@ import styles from "./app.module.css";
 import { useCallback, useEffect, useState } from "react";
 import VideoDetail from "./components/video_detail/video_detail";
 
-// 구현해 볼 것
-// 1. 브라우저창이 작아지면 리스트를 아래로 내리기 (media query 사용)
-// 2. UI 멋지게 구현해보기
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
@@ -18,13 +15,12 @@ function App({ youtube }) {
   const search = useCallback(
     (query) => {
       setSelectedVideo(null);
-      // 목록 받아오는 중에 로딩스피너 보여주기
 
       youtube
         .search(query) //
         .then((videos) => {
           setVideos(videos);
-        }); // 에러페이지를 만들고 싶다면 에러state를 만들어서 이를 업데이트
+        });
     },
     [youtube]
   );
